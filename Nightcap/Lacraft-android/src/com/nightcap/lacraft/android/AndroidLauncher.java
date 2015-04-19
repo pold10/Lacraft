15c41f701f4e2f80ca29a1ebe0043304cb259220
pold10 <myl.pem@gmail.com>
2015-04-19 00:15:29 -0300

    Proyecto creado, usando libGDX.
    Signed-off-by: pold10 <myl.pem@gmail.com>

---
+++ Nightcap/Lacraft-android/src/com/nightcap/lacraft/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nightcap.lacraft.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nightcap.lacraft.LacraftGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LacraftGame(), config);
+	}
+}
